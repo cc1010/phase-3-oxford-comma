a27ad8cf32e6ca5e27df688afec7be8ced1b5f61
chaim <chaimyisroel@gmail.com>
2021-09-19 17:32:41 -0500

    commit tree fe51936f12327013b48e1a14ab6819dd3db30e25

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        array.join(' ')
+    elsif  array.length == 2
+        array.join(' and ')
+    else
+        array.each_with_index.map { |word,i| i == (array.length - 1) ? "and #{word}" : word }.join(", ")
+    end
 end
+
+p  oxford_comma(["fiddleheads", "okra", "kohlrabi"])
